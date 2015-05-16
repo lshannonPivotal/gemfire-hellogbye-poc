e94238559bb22cb8cd3f88b2d3386637d510e310
lshannon <luke.shannon@gmail.com>
2015-05-16 04:07:37 -0400

    Adding logic to generate the data files remotely

--- ServerConfigs/scp.sh
+++ ServerConfigs/scp.sh
@@ -5,6 +5,7 @@ echo "1: Everything"
 echo "2: scripts"
 echo "3: conf"
 echo "4: lib"
+echo "5: data"
 read option
 if [[ ("$option" -eq "1") ]]; then
 	echo "Uploading the Gemfire package to all servers"
@@ -49,4 +50,15 @@ if [[ ("$option" -eq "4") ]]; then
 	scp -r -i gemfire.pem gemfire-ubuntu-package/lib ubuntu@$SERVER_1:/home/ubuntu/cluster/gemfire-ubuntu-package
 	echo "Uploading to Server 2"
 	scp -r -i gemfire.pem gemfire-ubuntu-package/lib ubuntu@$SERVER_2:/home/ubuntu/cluster/gemfire-ubuntu-package
+fi
+if [[ ("$option" -eq "5") ]]; then
+	echo "Uploading the data to all servers"
+	echo "Uploading to Locator 1"
+	scp -r -i gemfire.pem gemfire-ubuntu-package/data ubuntu@$LOCATOR_SERVER_1:/home/ubuntu/cluster/gemfire-ubuntu-package
+	echo "Uploading to Locator 2"
+	scp -r -i gemfire.pem gemfire-ubuntu-package/data ubuntu@$LOCATOR_SERVER_2:/home/ubuntu/cluster/gemfire-ubuntu-package
+	echo "Uploading to Server 1"
+	scp -r -i gemfire.pem gemfire-ubuntu-package/data ubuntu@$SERVER_1:/home/ubuntu/cluster/gemfire-ubuntu-package
+	echo "Uploading to Server 2"
+	scp -r -i gemfire.pem gemfire-ubuntu-package/data ubuntu@$SERVER_2:/home/ubuntu/cluster/gemfire-ubuntu-package
 fi
--- ServerConfigs/setup.sh
+++ ServerConfigs/setup.sh
@@ -3,6 +3,8 @@ source ./environment.sh
 echo "What set up?"
 echo "1: Create directories (new install)"
 echo "2: Open permissions on scripts"
+echo "3: Create data"
+echo "4: Delete data"
 read option
 if [[ ("$option" -eq "1") ]]; then
 	echo "Creating the base"
@@ -17,5 +19,21 @@ if [[ ("$option" -eq "2") ]]; then
 	ssh -i gemfire.pem ubuntu@$LOCATOR_SERVER_2 chmod +x /home/ubuntu/cluster/gemfire-ubuntu-package/scripts/*.sh
 	ssh -i gemfire.pem ubuntu@$SERVER_1 chmod +x /home/ubuntu/cluster/gemfire-ubuntu-package/scripts/*.sh
 	ssh -i gemfire.pem ubuntu@$SERVER_2 chmod +x /home/ubuntu/cluster/gemfire-ubuntu-package/scripts/*.sh
+fi
+if [[ ("$option" -eq "3") ]]; then
+	echo "How many copies?"
+	read total
+	ssh -i gemfire.pem ubuntu@$LOCATOR_SERVER_1 /home/ubuntu/cluster/gemfire-ubuntu-package/data/copydata.sh $total
+	ssh -i gemfire.pem ubuntu@$LOCATOR_SERVER_2 /home/ubuntu/cluster/gemfire-ubuntu-package/data/copydata.sh $total
+	ssh -i gemfire.pem ubuntu@$SERVER_1 /home/ubuntu/cluster/gemfire-ubuntu-package/data/copydata.sh $total
+	ssh -i gemfire.pem ubuntu@$SERVER_2 /home/ubuntu/cluster/gemfire-ubuntu-package/data/copydata.sh $total
+fi
+if [[ ("$option" -eq "4") ]]; then
+	echo "How many copies?"
+	read total
+	ssh -i gemfire.pem ubuntu@$LOCATOR_SERVER_1 /home/ubuntu/cluster/gemfire-ubuntu-package/data/deletedata.sh $total
+	ssh -i gemfire.pem ubuntu@$LOCATOR_SERVER_2 /home/ubuntu/cluster/gemfire-ubuntu-package/data/deletedata.sh $total
+	ssh -i gemfire.pem ubuntu@$SERVER_1 /home/ubuntu/cluster/gemfire-ubuntu-package/data/deletedata.sh $total
+	ssh -i gemfire.pem ubuntu@$SERVER_2 /home/ubuntu/cluster/gemfire-ubuntu-package/data/deletedata.sh $total
 fi
 
